5a9dbf1d518d5bc6ed486d0b97708f64edc9b94d
hRJ06 <hindolroy.2306@gmail.com>
2024-10-28 18:37:18 +0530

    feat: add config for chat using generative AI model

--- Client/src/lib/utils.ts
+++ Client/src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+/* CONFIGURATION FOR GENERATIVE MODEL */
+export const getModel = () => {
+  const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
+  const genAI = new GoogleGenerativeAI(`${API_KEY}`);
+  return genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+};
